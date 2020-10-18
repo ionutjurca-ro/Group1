73e0908800de644d593e49bb724a56d09463184b
Ionut Jurca <jurca_b@yahoo.com>
2020-10-18 15:05:19 +0300

    commit 18.10.2020_2

--- src/main/java/presentationLayer/Main.java
+++ src/main/java/presentationLayer/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
 //        Objects objects = new Objects();
 //        objects.createTaskObject();
+        // this is something modified
 
         startApp();
     }
